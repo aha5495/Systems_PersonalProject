1a599be51aa5ecefea38321fa62a586211c70c6d
aha5495 <151107011+aha5495@users.noreply.github.com>
2024-08-10 12:00:58 -0400

    Update Patron.c

--- Patron.c
+++ Patron.c
@@ -1,1 +1,11 @@
+/* Aravind H Alwar */
+/* August 10,2024 */
+//PATRON.C
 
+#ifndef PATRON_C_ //Define PATRON_C_
+#define PATRON_C_
+
+void set_User(char *F_Name,char *M_Name,char *L_Name){
+  char name=*F_Name+" "+*M_Name+" "+*L_Name; //Using pointers to efficently create the username;
+  char* userName=name[50]; //Use a pointer variable,userName to hold the value of name(up to 50 chars accepted);Setting the user's name
+}
